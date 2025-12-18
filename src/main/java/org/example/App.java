aedf573379182add5fbf25f70bd825583955437a
uhphuc <phucgaming611@gmail.com>
2025-12-18 21:41:49 +0700

    logo update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,8 +11,10 @@ public class App extends Application {
         FXMLLoader fxml = new FXMLLoader(App.class.getResource("/game/menu.fxml"));
         Scene scene = new Scene(fxml.load());
         stage.setScene(scene);
-        stage.setTitle("PacmanV Screen");
+        stage.setTitle("RoboVerse Menu");
+        stage.getIcons().add(new javafx.scene.image.Image(
+            App.class.getResourceAsStream("/images/logo.png")
+        ));
         stage.show();
     }
 }
-
